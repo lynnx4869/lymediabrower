4dd5cdedcf4a8c6cf70d4fb29538f6d35ca28ffd
LYninG <lynnx4869@gmail.com>
2020-10-15 00:36:17 +0800

    change config

--- Server/Configs.js
+++ Server/Configs.js
@@ -3,6 +3,11 @@
 const path = require('path');
 
 const modules = [{
+    itemName: 'TDDOWNLOAD',
+    itemPath: path.join(__dirname, '../../../../TDDOWNLOAD'),
+    playPath: '',
+    type: 'module'
+}, {
     itemName: '魔穗字幕组',
     itemPath: path.join(__dirname, '../../../../TDDOWNLOAD/魔穗字幕组'),
     playPath: '',
@@ -13,23 +18,8 @@ const modules = [{
     playPath: '',
     type: 'module'
 }, {
-    itemName: 'download',
-    itemPath: path.join('F:/download'),
-    playPath: '',
-    type: 'module'
-}, {
     itemName: '这种邪恶的东西',
-    itemPath: path.join('G:/这种邪恶的东西，不足为外人道也'),
-    playPath: '',
-    type: 'module'
-}, {
-    itemName: '动漫',
-    itemPath: path.join('G:/动漫'),
-    playPath: '',
-    type: 'module'
-}, {
-    itemName: '影视',
-    itemPath: path.join('G:/影视'),
+    itemPath: path.join('H:/这种邪恶的东西，不足为外人道也'),
     playPath: '',
     type: 'module'
 }];
